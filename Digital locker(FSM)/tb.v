5e4666bd62b5ce946252eb608f8df6a0e26d64e4
Atharva Verma <atharva02006@gmail.com>
2026-08-28 23:12:15 +0530

    Create tb.v

---
+++ Digital locker(FSM)/tb.v
@@ -0,0 +1,39 @@
+module tb();
+
+reg clk,rst,data_in,submit;
+wire locked,unlocked;
+
+locker dut (clk,rst,data_in,submit,locked,unlocked);
+
+initial begin
+    {clk,rst,data_in,submit} = 0;
+end
+
+always #5 clk=~clk;
+
+initial begin
+    @(negedge clk)
+    rst = 1'b1;
+
+    @(negedge clk)
+    rst = 1'b0;
+    data_in = 1'b1;
+
+    @(negedge clk)
+    data_in = 1'b0;
+
+    @(negedge clk)
+    data_in = 1'b1;
+
+    @(negedge clk)
+    data_in = 1'b0;
+    submit = 1'b1;
+end
+    
+initial begin
+    $dumpfile("waveform.vcd");
+    $dumpvars(0,tb);
+end
+
+
+endmodule
